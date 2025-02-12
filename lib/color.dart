232cb545f8004541b92f04de207f9b0f8bedefcc
ArasEmin <muhammedemin319@outlook.com>
2025-02-12 15:02:46 +0300

    15:03 12.02 M

---
+++ lib/color.dart
@@ -0,0 +1,4 @@
+const String koyuMor = "#493D9E";
+const String ortaMor = "#B2A5FF";
+const String acikMor = "#DAD2FF";
+const String arkaplanBeyaz = "#F5F5F5";
--- lib/user_login.dart
+++ lib/user_login.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:engelsizdolmus/color.dart';
 
-class UserLogin extends StatefulWidget {
+void main() {
+  runApp(const UserLogin());
+}
+
+class UserLogin extends StatelessWidget {
   const UserLogin({super.key});
 
   @override
-  State<UserLogin> createState() => _UserLoginScreenState();
-}
-
-class _UserLoginScreenState extends State<UserLogin> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: HexColor(arkaplanBeyaz),
+          body: Container(
+            width: deviceWidth,
+            height: deviceHeight / 3,
+            color: HexColor(koyuMor),
+            child: const Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: Text(
+                    "HOŞGELDİNİZ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Text(
+                    "Giriş Sayfası",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
